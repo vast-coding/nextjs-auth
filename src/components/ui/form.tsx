fee69bfcf9446be5cd79e5fd4ac766c10d305218
vast-coding <vast.coding@gmail.com>
2023-11-08 20:13:11 +0900

    remove label red text on error

--- src/components/ui/form.tsx
+++ src/components/ui/form.tsx
@@ -90,12 +90,7 @@ const FormLabel = React.forwardRef<
   const { error, formItemId } = useFormField()
 
   return (
-    <Label
-      ref={ref}
-      className={cn(error && 'text-destructive', className)}
-      htmlFor={formItemId}
-      {...props}
-    />
+    <Label ref={ref} className={className} htmlFor={formItemId} {...props} />
   )
 })
 FormLabel.displayName = 'FormLabel'
